69e399af49ce441eff2ae8640b2c7617ce568464
Willy Tarreau <w@1wt.eu>
2023-11-26 16:58:02 +0100

    TESTS: testcba: indicate that a duplicate node is being removed
    It's important to indicate it's being removed so that we know what to
    expect to find in the tree. Let's do it as well for testcbamb.

--- tests/testcbamb.c
+++ tests/testcbamb.c
@@ -34,6 +34,7 @@ struct cba_node *add_value(struct cba_node **root, uint32_t value)
 		if (prev == &key->node)
 			return prev; // was properly inserted
 		/* otherwise was already there, let's try to remove it */
+		printf("Insert failed, removing node %p before inserting again.\n", prev);
 		ret = cba_delete_mb(root, prev, sizeof(uint32_t));
 		if (ret != prev) {
 			/* was not properly removed either: THIS IS A BUG! */
