2a0ad1f58dcea02acfc05b3977fd63e2e10cf3fe
Willy Tarreau <w@1wt.eu>
2023-12-17 21:56:57 +0100

    TESTS: u64: also add some simple test cases for 64 bit keys
    This is essentially a copy-paste of the 32 with s/32/64 with printf
    formats adjusted.

---
+++ tests/testcbu64.c
@@ -0,0 +1,97 @@
+#include <sys/time.h>
+
+#include <inttypes.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "cbu64_tree.h"
+
+struct cb_node *cb_root = NULL;
+
+struct key {
+	struct cb_node node;
+	uint64_t key;
+};
+
+struct cb_node *add_value(struct cb_node **root, uint64_t value)
+{
+	struct key *key;
+	struct cb_node *prev, *ret;
+
+	key = calloc(1, sizeof(*key));
+	key->key = value;
+	do {
+		prev = cbu64_insert(root, &key->node);
+		if (prev == &key->node)
+			return prev; // was properly inserted
+		/* otherwise was already there, let's try to remove it */
+		printf("Insert failed, removing node %p before inserting again.\n", prev);
+		ret = cbu64_delete(root, prev);
+		if (ret != prev) {
+			/* was not properly removed either: THIS IS A BUG! */
+			printf("failed to insert %p(%llu) because %p has the same key and could not be removed because returns %p\n",
+			       &key->node, (unsigned long long)key->key, prev, ret);
+			free(key);
+			return NULL;
+		}
+		free(container_of(ret, struct key, node));
+	} while (1);
+}
+
+int main(int argc, char **argv)
+{
+	const struct cb_node *old;
+	char *argv0 = *argv, *larg;
+	char *orig_argv;
+	char *p;
+	uint64_t v;
+	int debug = 0;
+
+	argv++; argc--;
+
+	while (argc && **argv == '-') {
+		if (strcmp(*argv, "-d") == 0)
+			debug++;
+		else {
+			printf("Usage: %s [-d]* [value]*\n", argv0);
+			exit(1);
+		}
+		argc--; argv++;
+	}
+
+	orig_argv = larg = *argv;
+	while (argc > 0) {
+		v = atoll(argv[0]);
+		old = cbu64_lookup(&cb_root, v);
+		if (old)
+			fprintf(stderr, "Note: value %llu already present at %p\n", (unsigned long long)v, old);
+		old = add_value(&cb_root, v);
+
+		if (debug) {
+			static int round;
+			char cmd[100];
+			size_t len;
+
+			len = snprintf(cmd, sizeof(cmd), "%s [%d] +%llu", orig_argv, round, (unsigned long long)v);
+			cbu64_default_dump(&cb_root, len < sizeof(cmd) ? cmd : orig_argv, old);
+			round++;
+		}
+
+		argv++;
+		argc--;
+	}
+
+	/* rebuild non-debug args as a single string */
+	for (p = orig_argv; p < larg; *p++ = ' ')
+		p += strlen(p);
+
+	if (!debug)
+		cbu64_default_dump(&cb_root, orig_argv, 0);
+
+	return 0;
+}
