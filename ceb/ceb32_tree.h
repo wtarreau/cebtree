3d8aff575e1f88c55549cad6aa6863c46d4e12b3
Willy Tarreau <w@1wt.eu>
2025-01-05 19:58:45 +0100

    MEDIUM: ceb32: change the ceb32_{next,prev} to use the full ones
    Now the ceb32_next() and ceb32_prev() functions properly visit duplicates.
    ceb32_next_unique() and ceb32_prev_unique() are now available to continue
    to visit unique keys.

--- ceb/ceb32_tree.h
+++ ceb/ceb32_tree.h
@@ -36,6 +36,8 @@ struct ceb_node *ceb32_lookup_le(struct ceb_node **root, uint32_t key);
 struct ceb_node *ceb32_lookup_lt(struct ceb_node **root, uint32_t key);
 struct ceb_node *ceb32_lookup_ge(struct ceb_node **root, uint32_t key);
 struct ceb_node *ceb32_lookup_gt(struct ceb_node **root, uint32_t key);
+struct ceb_node *ceb32_next_unique(struct ceb_node **root, struct ceb_node *node);
+struct ceb_node *ceb32_prev_unique(struct ceb_node **root, struct ceb_node *node);
 struct ceb_node *ceb32_next(struct ceb_node **root, struct ceb_node *node);
 struct ceb_node *ceb32_prev(struct ceb_node **root, struct ceb_node *node);
 struct ceb_node *ceb32_delete(struct ceb_node **root, struct ceb_node *node);
@@ -51,6 +53,8 @@ struct ceb_node *ceb32_ofs_lookup_le(struct ceb_node **root, ptrdiff_t kofs, uin
 struct ceb_node *ceb32_ofs_lookup_lt(struct ceb_node **root, ptrdiff_t kofs, uint32_t key);
 struct ceb_node *ceb32_ofs_lookup_ge(struct ceb_node **root, ptrdiff_t kofs, uint32_t key);
 struct ceb_node *ceb32_ofs_lookup_gt(struct ceb_node **root, ptrdiff_t kofs, uint32_t key);
+struct ceb_node *ceb32_ofs_next_unique(struct ceb_node **root, ptrdiff_t kofs, struct ceb_node *node);
+struct ceb_node *ceb32_ofs_prev_unique(struct ceb_node **root, ptrdiff_t kofs, struct ceb_node *node);
 struct ceb_node *ceb32_ofs_next(struct ceb_node **root, ptrdiff_t kofs, struct ceb_node *node);
 struct ceb_node *ceb32_ofs_prev(struct ceb_node **root, ptrdiff_t kofs, struct ceb_node *node);
 struct ceb_node *ceb32_ofs_delete(struct ceb_node **root, ptrdiff_t kofs, struct ceb_node *node);
