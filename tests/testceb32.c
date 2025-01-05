438cac72b1ce4cf38f92f93c4677ee152d6a2a35
Willy Tarreau <w@1wt.eu>
2025-01-05 19:54:41 +0100

    MINOR: tests: make testceb32 dump all entries at the end
    The test now emits all entries in direct and reverse order using
    ceb_first()+ceb_next() and ceb_last()+ceb_prev(). It's produced as
    a comment so that it can remain in the dot file for later checks as
    well.
    
    Note that at the moment only unique keys are dumped (with the last
    one of each dup subtree).

--- tests/testceb32.c
+++ tests/testceb32.c
@@ -51,6 +51,7 @@ int main(int argc, char **argv)
 	char *p;
 	uint32_t v;
 	int debug = 0;
+	int i;
 
 	argv++; argc--;
 
@@ -98,6 +99,14 @@ int main(int argc, char **argv)
 		ceb32_default_dump(0, 0, 0, 0); // epilogue
 	else
 		ceb32_default_dump(&ceb_root, orig_argv, 0, 0);
+
+	printf("# Dump of all nodes using first() + next()\n");
+	for (i = 0, old = ceb32_first(&ceb_root); old; i++, old = ceb32_next(&ceb_root, (struct ceb_node*)old))
+		printf("# node[%d]=%p key=%u\n", i, old, container_of(old, struct key, node)->key);
+
+	printf("# Dump of all nodes using last() + prev()\n");
+	for (i = 0, old = ceb32_last(&ceb_root); old; i++, old = ceb32_prev(&ceb_root, (struct ceb_node*)old))
+		printf("# node[%d]=%p key=%u\n", i, old, container_of(old, struct key, node)->key);
 
 	return 0;
 }
