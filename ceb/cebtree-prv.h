e429522712ab72c0a3bab8d420f1c3decee79bd3
Willy Tarreau <w@1wt.eu>
2025-01-05 15:30:17 +0100

    MEDIUM: descent: stop on duplicates during the descent
    Till now we would stop on duplicates when the xor between the branches
    increases again, but that's not compatible with the use of duplicates.
    Indeed, this can bring us too far, which is particularly visible during
    insertion where the new node is inserted at the bottom of the dup list.
    
    Just like we're doing with the detection of leaves, we need to stop as
    soon as we're certain we're on a dup. That's actually guaranteed by a
    xor value of zero, or a full-size equality between the two sides. And
    we always have that info since the xor between the two sides is always
    calculated.
    
    We also need to make the nodeless node check more accurate for the
    keyless walk otherwise the next walk can wrongly stop on a two-dups
    sub-tree believing it found the nodeless node. A performance test would
    be useful here to see if checking (l==r && l==k) could be faster than
    (l==k && r==k) due to the first condition being rare.
    
    Since this adds extra tests, it might possibly slightly decrease the
    performance in the descent, but given that it's bound by the memory
    bandwidth anyway, it shouldn't have much impact. Also for the string
    case we could later improve this by folding the two consecutive tests
    by letting the compiler know that (ssize_t)plen is always positive, but
    that's out of scope for now.

--- ceb/cebtree-prv.h
+++ ceb/cebtree-prv.h
@@ -475,8 +475,12 @@ struct ceb_node *_ceb_descend(struct ceb_node **root,
 
 		dbg(__LINE__, "newp", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
 
-		/* two equal pointers identifies the nodeless leaf. */
-		if (l == r) {
+		/* two equal pointers identifies either the nodeless leaf or
+		 * the 2nd dup of a sub-tree. Both are leaves anyway, but we
+		 * must not yet stop here for a dup as we may want to report
+		 * it.
+		 */
+		if (l == k && r == k) {
 			dbg(__LINE__, "l==r", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
 			break;
 		}
@@ -533,6 +537,12 @@ struct ceb_node *_ceb_descend(struct ceb_node **root,
 			kl = l->u32; kr = r->u32;
 			xor32 = kl ^ kr;
 
+			if (!xor32) {
+				/* exact match, that's a duplicate */
+				dbg(__LINE__, "dup>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
+				break;
+			}
+
 			if (xor32 > pxor32) { // test using 2 4 6 4
 				dbg(__LINE__, "xor>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
 				break;
@@ -566,6 +576,12 @@ struct ceb_node *_ceb_descend(struct ceb_node **root,
 
 			kl = l->u64; kr = r->u64;
 			xor64 = kl ^ kr;
+
+			if (!xor64) {
+				/* exact match, that's a duplicate */
+				dbg(__LINE__, "dup>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
+				break;
+			}
 
 			if (xor64 > pxor64) { // test using 2 4 6 4
 				dbg(__LINE__, "xor>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
@@ -607,6 +623,12 @@ struct ceb_node *_ceb_descend(struct ceb_node **root,
 			}
 
 			xlen = equal_bits(l->mb, r->mb, 0, key_u64 << 3);
+			if (xlen == key_u64 << 3) {
+				/* exact match, that's a duplicate */
+				dbg(__LINE__, "dup>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
+				break;
+			}
+
 			if (xlen < plen) {
 				/* this is a leaf. E.g. triggered using 2 4 6 4 */
 				dbg(__LINE__, "xor>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
@@ -650,6 +672,12 @@ struct ceb_node *_ceb_descend(struct ceb_node **root,
 			}
 
 			xlen = equal_bits(l->ptr, r->ptr, 0, key_u64 << 3);
+			if (xlen == key_u64 << 3) {
+				/* exact match, that's a duplicate */
+				dbg(__LINE__, "dup>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
+				break;
+			}
+
 			if (xlen < plen) {
 				/* this is a leaf. E.g. triggered using 2 4 6 4 */
 				dbg(__LINE__, "xor>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
@@ -698,6 +726,12 @@ struct ceb_node *_ceb_descend(struct ceb_node **root,
 			}
 
 			xlen = string_equal_bits(l->str, r->str, 0);
+			if ((ssize_t)xlen < 0) {
+				/* exact match, that's a duplicate */
+				dbg(__LINE__, "dup>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
+				break;
+			}
+
 			if (xlen < plen) {
 				/* this is a leaf. E.g. triggered using 2 4 6 4 */
 				dbg(__LINE__, "xor>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
@@ -747,6 +781,12 @@ struct ceb_node *_ceb_descend(struct ceb_node **root,
 			}
 
 			xlen = string_equal_bits(l->ptr, r->ptr, 0);
+			if ((ssize_t)xlen < 0) {
+				/* exact match, that's a duplicate */
+				dbg(__LINE__, "dup>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
+				break;
+			}
+
 			if (xlen < plen) {
 				/* this is a leaf. E.g. triggered using 2 4 6 4 */
 				dbg(__LINE__, "xor>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
@@ -784,6 +824,12 @@ struct ceb_node *_ceb_descend(struct ceb_node **root,
 
 			kl = (uintptr_t)l; kr = (uintptr_t)r;
 			xoraddr = kl ^ kr;
+
+			if (!xoraddr) {
+				/* exact match, that's a duplicate */
+				dbg(__LINE__, "dup>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
+				break;
+			}
 
 			if (xoraddr > (uintptr_t)pxor64) { // test using 2 4 6 4
 				dbg(__LINE__, "xor>", meth, kofs, key_type, root, p, key_u32, key_u64, key_ptr, pxor32, pxor64, plen);
