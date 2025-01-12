a82414274a3e61b453b76ea685cbf6eb8be217c9
Willy Tarreau <w@1wt.eu>
2025-01-12 14:40:17 +0100

    MEDIUM: private: factor the unique and dup versions of lookup()
    The lookup() call is very similar between the two versions. The dup-aware
    one only has an extra check based on is_dup. Passing its pointer as an
    extra argument to the _ceb_lookup() function permits to condition the
    dup-specific code based on the pointer passed by the caller. Thus we
    now rely on the caller to pass that value, and we can get rid of
    _cebu_lookup(). It was verified that the functions are exactly the same
    size before and after.

--- ceb/cebis_tree.c
+++ ceb/cebis_tree.c
@@ -78,7 +78,9 @@ CEB_FDECL2(struct ceb_node *, cebis, _last, struct ceb_node **, root, ptrdiff_t,
  */
 CEB_FDECL3(struct ceb_node *, cebis, _lookup, struct ceb_node **, root, ptrdiff_t, kofs, const void *, key)
 {
-	return _ceb_lookup(root, kofs, CEB_KT_IS, 0, 0, key);
+	int is_dup = 0;
+
+	return _ceb_lookup(root, kofs, CEB_KT_IS, 0, 0, key, &is_dup);
 }
 
 /* look up the specified key or the highest below it, and returns either the
@@ -239,7 +241,7 @@ CEB_FDECL2(struct ceb_node *, cebuis, _last, struct ceb_node **, root, ptrdiff_t
  */
 CEB_FDECL3(struct ceb_node *, cebuis, _lookup, struct ceb_node **, root, ptrdiff_t, kofs, const void *, key)
 {
-	return _cebu_lookup(root, kofs, CEB_KT_IS, 0, 0, key);
+	return _ceb_lookup(root, kofs, CEB_KT_IS, 0, 0, key, NULL);
 }
 
 /* look up the specified key or the highest below it, and returns either the
