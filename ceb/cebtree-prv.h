wtarreau/cebtree

commit 09902b8078c54dbc39bf91192049321685d22fbf

MINOR: private: improve string_equal_bits to optimize the loop
By moving the loads earlier and pre-loading the next cycle, we
can speed the function up a little bit (~1%) and further reduce
the code by about 20%.

Attempts were made at loading 3 and 4 bytes per iterations, but
there are diminishing returns and it only makes sense when the
function is not inlined. However, there's a 1-2% performance
loss when it's not inlined so it's not worth the pain. But in
this case the code is further reduced, which could be a possible
choice based on the code optimization.

ceb/cebtree-prv.h
@@ -287,34 +287,34 @@ size_t string_equal_bits(const unsigned char *a,
 	/* skip known and identical bits. We stop at the first different byte
 	 * or at the first zero we encounter on either side.
 	 */
+	c = a[beg + 0];
 	for (;; beg += 2) {
-		c = a[beg + 0];
 		d = b[beg + 0];
 		c ^= d;
 		if (__builtin_expect(c, 0))
 			goto brk1;
+		c = a[beg + 1];
 		if (__builtin_expect(!d, 0))
 			goto same;
 
-		c = a[beg + 1];
 		d = b[beg + 1];
 		c ^= d;
 		if (__builtin_expect(c, 0))
 			goto brk2;
+		c = a[beg + 2];
 		if (__builtin_expect(!d, 0))
 			goto same;
 	}
 brk2:
 	beg++;
 brk1:
-	beg++;
 
-	/* OK now we know that a and b differ at byte <beg>, or that both are zero.
+	/* OK now we know that a and b differ at byte <beg+1>, or that both are zero.
 	 * We have to find what bit is differing and report it as the number of
 	 * identical bits. Note that low bit numbers are assigned to high positions
 	 * in the byte, as we compare them as strings.
 	 */
-	return (beg << 3) + ((flsnz(c) - 1) ^ 7) - 8;
+	return (beg << 3) + ((flsnz(c) - 1) ^ 7);
 same:
 	return (size_t)-1;
 }

commit c7a0eebb71ea2dd944986b4e0aec1baf339a98f8

BUG/MEDIUM: private: never read past end in string_equal_bits()
The change in commit 09902b8 ("MINOR: private: improve string_equal_bits
to optimize the loop") accidentally resulted in reading byte N+1 before
knowing if byte N was zero or not, which was caught by ASAN.

Let's partially undo that fix while still checking early if we can leave
the loop. In the end the code is now even 4% faster on older compilers
(gcc-9.5 and below) while also quite a bit larger. On newer compilers
(11 and above) which always produce significantly slower code than old
ones, there's no observable difference, so the code was written in a
way that the compiler will have no problem optimizing, giving a good
balance of performance on all of them.

It's interesting to note that by just assigning "c" first before
entering the loop then at the end of the loop, the code becomes
much bigger on older compilers and significantly faster at the same
time (~1%), but this approach doesn't work on new ones.

ceb/cebtree-prv.h
@@ -297,29 +297,27 @@ size_t string_equal_bits(const unsigned char *a,
 	/* skip known and identical bits. We stop at the first different byte
 	 * or at the first zero we encounter on either side.
 	 */
-	c = a[beg + 0];
 	for (;; beg += 2) {
+		c = a[beg + 0];
 		d = b[beg + 0];
 		c ^= d;
-		if (__builtin_expect(c, 0))
+		if (__builtin_expect(c != 0, 0))
 			goto brk1;
-		c = a[beg + 1];
-		if (__builtin_expect(!d, 0))
+		if (!d)
 			goto same;
-
+		c = a[beg + 1];
 		d = b[beg + 1];
 		c ^= d;
-		if (__builtin_expect(c, 0))
+		if (__builtin_expect(c != 0, 0))
 			goto brk2;
-		c = a[beg + 2];
-		if (__builtin_expect(!d, 0))
+		if (!d)
 			goto same;
 	}
 brk2:
 	beg++;
 brk1:
 
-	/* OK now we know that a and b differ at byte <beg+1>, or that both are zero.
+	/* OK now we know that a and b differ at byte <beg>.
 	 * We have to find what bit is differing and report it as the number of
 	 * identical bits. Note that low bit numbers are assigned to high positions
 	 * in the byte, as we compare them as strings.